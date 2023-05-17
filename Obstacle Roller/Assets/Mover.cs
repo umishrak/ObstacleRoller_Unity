d06bdc6412da738eb4ff85a720b9500e805ab2a4
Utpal Kant Mishra <authorutpal@gmail.com>
2023-05-17 21:03:50 +0530

    Axis Input Event Implemented

--- Obstacle Roller/Assets/Mover.cs
+++ Obstacle Roller/Assets/Mover.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
-{
-    [SerializeField] float xValue = 0; 
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0;
-    
+{ 
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +12,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue);
         
     }
 }
