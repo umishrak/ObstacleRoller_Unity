bcb3019625635b6d6ad301f14beaaf95547c16ef
Utpal Kant Mishra <authorutpal@gmail.com>
2023-05-18 15:23:49 +0530

    Move Player and Print Log Functions (Methods) implemented

--- Obstacle Roller/Assets/Mover.cs
+++ Obstacle Roller/Assets/Mover.cs
@@ -8,15 +8,25 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();        
+    }
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the Game!"); 
+        Debug.Log("Use WASD or Arrow Keys to move the player.");
+        Debug.Log("Keep away from the walls!");
+    }
 
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
-        
     }
 }
