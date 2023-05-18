adeb12fdd70bdcd70db9a9c56f51930f51821c20
Utpal Kant Mishra <authorutpal@gmail.com>
2023-05-18 18:40:33 +0530

    OnCollisionEnter function implemented - Warning when Player hit the walls

---
+++ Obstacle Roller/Assets/ObjectHit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("ObjectHit");
+                
+    }
+
+}
